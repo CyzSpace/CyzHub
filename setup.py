fd4ffbcb8fb681249321388ac1c05c80d4f91b9c
RootKai <67811039+CyzSpace@users.noreply.github.com>
2024-10-03 22:09:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='陈颍州',  # 作者名
     author_email='cyzroot@outlook.com',  # 作者邮箱
     description='深度学习的一些工具（基础阶段的，供参考学习使用）',  # 简短描述
-    long_description=open('README.md').read(),  # 从 README.md 中读取详细描述
+    long_description=open('README.md', 'r', encoding='utf-8').read(),  # 从 README.md 中读取详细描述
     long_description_content_type='text/markdown',  # 描述的格式
     url='https://github.com/CyzSpace/CyzHub',  # 项目的URL
     packages=find_packages(),  # 自动查找项目中的所有包
